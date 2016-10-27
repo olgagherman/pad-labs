37e9e89a1152d4b83977d30a0606b522cb0fb25d
Gherman Olga <gherman.olya@gmail.com>
2016-10-27 16:39:42 +0300

    changes student attributes

--- classes/students.py
+++ classes/students.py
@@ -1,4 +1,5 @@
 class Discipline:
-    def __init__(self, name, group):
+    def __init__(self, name, group, mark):
         self.name = name
         self.academic_group = group
+        self.average_mark = mark
