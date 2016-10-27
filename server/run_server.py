6dacf314be873d756754b72a8979d39837a59d51
Gherman Olga <gherman.olya@gmail.com>
2016-10-27 16:43:09 +0300

    adds config dictionary

--- server/run_server.py
+++ server/run_server.py
@@ -1,8 +1,43 @@
 import asyncio
+import collections
+
+_CONFIG = {
+    1: {
+        "neighbor": [2],
+        "port": 9001,
+        "host": "127.0.0.1"
+    },
+    2: {
+        "neighbor": [1, 3],
+        "port": 9002,
+        "host": "127.0.0.1"
+    },
+    3: {
+        "neighbor": [2, 4],
+        "port": 9003,
+        "host": "127.0.0.1"
+    },
+    4: {
+        "neighbor": [3, 5, 6],
+        "port": 9004,
+        "host": "127.0.0.1"
+    },
+    5: {
+        "neighbor": [4],
+        "port": 9005,
+        "host": "127.0.0.1"
+    },
+    6: {
+        "neighbor": [4],
+        "port": 9006,
+        "host": "127.0.0.1"
+    }
+}
+
+_SERVERS = []
 
 #point of entry for servers
 def run_server():
-
 
 if __name__ == "__main__":
     run_server()
