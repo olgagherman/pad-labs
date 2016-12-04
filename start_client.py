35002b8679306025dda7758ed7f3714092ed036f
Alexandr G <alexandr@gavrisco.com>
2016-12-04 18:37:04 +0200

    Send loop to the client

--- start_client.py
+++ start_client.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3.5
+import asyncio
 from client import Client
 
 
 if __name__ == "__main__":
-    client = Client()
+    client = Client(asyncio.get_event_loop())
     client.run()
