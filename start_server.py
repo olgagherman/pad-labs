cd75ea6d597a9d31ebae89aa7a15a12ef3cc6db1
Alexandr G <alexandr@gavrisco.com>
2016-12-04 19:13:56 +0200

    Update startup of the server

--- start_server.py
+++ start_server.py
@@ -1,4 +1,19 @@
-from server.run_server import run_server
+#!/usr/bin/env python3.5
+import asyncio
+import logging
+
+from server import ServerNode
+from core import settings
+
+
+logging.config.dictConfig(settings.LOGGING_CONFIG)
+
 
 if __name__ == "__main__":
-    run_server()
+    node = ServerNode(
+        asyncio.get_event_loop(),
+        udp_group_address=settings.UDP_GROUP_ADDRESS,
+        udp_group_port=settings.UDP_GROUP_PORT,
+        **settings.SERVER_CONFIG[1],
+    )
+    node.run()
