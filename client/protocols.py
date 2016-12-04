9ec2a9a7c8ec0b4cc8b5e6444d139451db062dab
Alexandr G <alexandr@gavrisco.com>
2016-12-04 18:46:16 +0200

    Update udp request protocolo

--- client/protocols.py
+++ client/protocols.py
@@ -1,18 +1,20 @@
+import asyncio
 import logging
-from asyncio import Protocol
+import socket
+import struct
 
 
 LOGGER = logging.getLogger(__name__)
 
 
-class BaseClientProtocol(Protocol):
+class BaseClientProtocol(object):
     '''
     Base client Protocol.
 
     This base class provides some methods with default implementation.
     `client` is an instance of `client.app.Client` for control of the flow.
     '''
-    def __init__(self, client):
+    def __init__(self, client, *args, **kwargs): # pylint: disable=unused-argument
         self.client = client
 
     def error_received(self, exc):
@@ -22,11 +24,23 @@ class BaseClientProtocol(Protocol):
         LOGGER.error('Connection Lost %s', exc)
 
 
-class ClientRequestUdpProtocol(BaseClientProtocol):
+class ClientRequestUdpProtocol(BaseClientProtocol, asyncio.DatagramProtocol):
     '''
     Protocol which implements requesting of data from nodes via multicast
     '''
+    def __init__(self, client, udp_address_group, *args, **kwargs):
+        super().__init__(client, *args, **kwargs)
+        self.udp_address_group = udp_address_group
+
+    def init_multicast_transport(self, transport):
+        sock = transport.get_extra_info('socket')
+        group = socket.inet_aton('239.255.255.250')
+        mreq = struct.pack('4sL', group, socket.INADDR_ANY)
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+
     def connection_made(self, transport):
+        self.init_multicast_transport(transport)
+
         message = self.client.get_info_request_message()
         LOGGER.debug('Connection made. Transport %s, Sending - %s', transport, message)
         transport.sendto(message)
@@ -34,7 +48,7 @@ class ClientRequestUdpProtocol(BaseClientProtocol):
         self.client.info_request_sent()
 
 
-class ClientResponseUdpProtocol(BaseClientProtocol):
+class ClientResponseUdpProtocol(BaseClientProtocol, asyncio.DatagramProtocol):
     '''
     Protocol which implements listening of info packets from nodes via unicast
     '''
@@ -42,7 +56,7 @@ class ClientResponseUdpProtocol(BaseClientProtocol):
         LOGGER.debug('Received %s from %s', data, addr)
         self.client.process_info_response(data)
 
-class ClientRequestTcpProtocol(BaseClientProtocol):
+class ClientRequestTcpProtocol(BaseClientProtocol, asyncio.Protocol):
     '''
     Protocl which implements retrieving of data from a node via TCP
     '''
